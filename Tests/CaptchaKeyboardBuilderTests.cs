fa695d44bac48e452c108a9fd019429a2ffb1b93
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-12-09 10:21:21 +0100

    Update CaptchaKeyboardBuilderTests.cs

--- Tests/CaptchaKeyboardBuilderTests.cs
+++ Tests/CaptchaKeyboardBuilderTests.cs
@@ -2,22 +2,24 @@ using System.Linq;
 using DruzhokBot.Common.Helpers;
 using Xunit;
 
-namespace Tests
+namespace Tests;
+
+public class CaptchaKeyboardBuilderTests
 {
-    public class CaptchaKeyboardBuilderTests
+    [Fact]
+    public void AssertCaptchaKeyboardBuilder_ShouldContainExpectedValues()
     {
-        [Fact]
-        public void AssertCaptchaKeyboardBuilder_ShouldContainExpectedValues()
-        {
-            var keyboardMarkup = CaptchaKeyboardBuilder.BuildCaptchaKeyboard(123);
+        // Arrange
+        var keyboardMarkup = CaptchaKeyboardBuilder.BuildCaptchaKeyboard(123);
 
-            var keyboardButtons = keyboardMarkup
-                .InlineKeyboard
-                .SelectMany(x => x)
-                .ToList();
+        // Act
+        var keyboardButtons = keyboardMarkup
+            .InlineKeyboard
+            .SelectMany(x => x)
+            .ToList();
 
-            Assert.True(keyboardButtons.Count(x => x.CallbackData!.Contains("new_user")) == 1);
-            Assert.True(keyboardButtons.Count(x => x.CallbackData!.Contains("ban_user")) == 5);
-        }
+        // Asserts
+        Assert.True(keyboardButtons.Count(x => x.CallbackData!.Contains("new_user")) == 1);
+        Assert.True(keyboardButtons.Count(x => x.CallbackData!.Contains("ban_user")) == 5);
     }
 }
